ae574cd2dd88d5ea439a8c1668c33f826816e0d8
Samuel Rodriguez <samuelyret@gmail.com>
2016-12-29 12:46:24 +0100

    create model and migration trainer

--- db/migrate/20161229114542_create_trainers.rb
+++ db/migrate/20161229114542_create_trainers.rb
@@ -1,12 +1,11 @@
-class CreateUsers < ActiveRecord::Migration[5.0]
+class CreateTrainers < ActiveRecord::Migration[5.0]
   def change
-    create_table :users do |t|
+    create_table :trainers do |t|
       t.string :first_name
       t.string :last_name
       t.string :pic_url
       t.integer :phone_number
       t.string :email
-      # t.references :trainer, foreign_key: true
 
       t.timestamps
     end
