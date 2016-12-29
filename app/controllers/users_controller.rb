437f31030842260104f99c72def11cce384527ad
Samuel Rodriguez <samuelyret@gmail.com>
2016-12-29 20:20:08 +0100

    new message now workin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user
 
+  def index
+    @users = User.order('created_at DESC').paginate(page: params[:page], per_page: 30)
+  end
+
   private
 
   def set_user
--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,7 +2,7 @@ module MessagesHelper
   def recipients_options(chosen_recipient = nil)
     s = ''
     User.all.each do |user|
-      s << "<option value='#{user.id}'>#{user.first_name}</option>"
+      s << "<option value='#{user.id}' #{'selected' if user == chosen_recipient}>#{user.first_name}</option>"
     end
     s.html_safe
   end
