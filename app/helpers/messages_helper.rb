83e6c5c99bb9ac24ca8658ef27e8f49c84e7ca76
Samuel Rodriguez <samuelyret@gmail.com>
2016-12-30 14:14:01 +0100

    fixed recipients to no admin can receive messages

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,7 +2,7 @@ module MessagesHelper
   def recipients_options(chosen_recipient = nil)
     s = ''
     User.all.each do |user|
-      if user != current_user
+       if user.admin != true && user != current_user
         s << "<option value='#{user.id}' #{'selected' if user == chosen_recipient}>#{user.first_name} #{user.last_name} - #{user.role}</option>"
       end
     end
