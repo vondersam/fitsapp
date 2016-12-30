bdcb0dd2b1b4cd0e8ce07ab2bb87bb13a340fd3d
Samuel Rodriguez <samuelyret@gmail.com>
2016-12-30 09:03:02 +0100

    finish navbar mobile/desktop

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ samuel = User.new
 samuel.first_name = "Samuel"
 samuel.last_name = "Horn"
 samuel.email = "samuel@horn.com"
-samuel.pic_url = open("http://res.cloudinary.com/bookaround/image/upload/v1481850165/samuelH_profile_pic_y0wzk8.jpg")
+samuel.pic_url = "http://lorempixel.com/400/400/people/"
 samuel.password = "FUBARS"
 samuel.password_confirmation = "FUBARS"
 samuel.valid?
@@ -21,7 +21,7 @@ gregory = User.new
 gregory.first_name = "Gregory"
 gregory.last_name = "Griffin"
 gregory.email = "gregory@griffin.com"
-gregory.pic_url = open("http://res.cloudinary.com/bookaround/image/upload/v1481752500/Gregory_profile_pic_pnmx8s.jpg")
+gregory.pic_url = "http://lorempixel.com/400/400/people/"
 gregory.password = "FUBARS"
 gregory.password_confirmation = "FUBARS"
 gregory.valid?
@@ -31,7 +31,7 @@ jasper = User.new
 jasper.first_name = "Jasper"
 jasper.last_name = "Hendrickx"
 jasper.email = "jasper@hendrickx.com"
-jasper.pic_url = open("http://res.cloudinary.com/bookaround/image/upload/v1481752496/jasper_profile_pic_segwtd.jpg")
+jasper.pic_url = "http://lorempixel.com/400/400/people/"
 jasper.password = "FUBARS"
 jasper.password_confirmation = "FUBARS"
 jasper.valid?
@@ -41,7 +41,7 @@ samuelbrown = Trainer.new
 samuelbrown.first_name = "Samuel"
 samuelbrown.last_name = "Rodriguez"
 samuelbrown.email = "samuel@rodriguez.com"
-samuelbrown.pic_url = open("http://res.cloudinary.com/bookaround/image/upload/v1481752506/SamuelR_lo1yq7.jpg")
+samuelbrown.pic_url = "http://lorempixel.com/400/400/people/"
 samuelbrown.password = "FUBARS"
 samuelbrown.password_confirmation = "FUBARS"
 samuelbrown.valid?
@@ -51,7 +51,7 @@ random = Trainer.new
 random.first_name = "Henrich"
 random.last_name = "Fruchart"
 random.email = "henrich@fruchart.com"
-random.pic_url = open("http://absorbmarketing.com/wp-content/uploads/2015/01/Picture-of-person.png")
+random.pic_url = "http://lorempixel.com/400/400/people/"
 random.password = "FUBARS"
 random.password_confirmation = "FUBARS"
 random.valid?
