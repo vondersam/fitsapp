b717a9cc582f2ec46754bc34ede0d9940e61e178
Samuel Rodriguez <samuelyret@gmail.com>
2016-12-30 14:01:23 +0100

    fix recipients so you cannot chat with yourself

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,7 +2,9 @@ module MessagesHelper
   def recipients_options(chosen_recipient = nil)
     s = ''
     User.all.each do |user|
-      s << "<option value='#{user.id}' #{'selected' if user == chosen_recipient}>#{user.first_name} #{user.last_name} - #{user.role}</option>"
+      if user != current_user
+        s << "<option value='#{user.id}' #{'selected' if user == chosen_recipient}>#{user.first_name} #{user.last_name} - #{user.role}</option>"
+      end
     end
     s.html_safe
   end
