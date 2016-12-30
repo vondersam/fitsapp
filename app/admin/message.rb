6dbf8c85c8574f009f7f1485a5343018972ad7c9
Samuel Rodriguez <samuelyret@gmail.com>
2016-12-30 23:25:57 +0100

    added bottom margin to messages css

--- app/admin/message.rb
+++ app/admin/message.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register Message do
-  permit_params :sender, :recipient, :content
-    # index do
-  #     selectable_column
-  #     column :id
-  #     column :sender
-  #     column :recipient
-  #     column :created_at
-  #     actions
-  # end
+  permit_params :sender, :recipient, :subject, :content
+    index do
+      selectable_column
+      column :id
+      column :sender
+      column :subject
+      column :recipient
+      column :created_at
+      actions
+  end
 end
