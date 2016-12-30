9a819bf173cfc1fae53faff72c9db0266e67a5a7
Samuel Rodriguez <samuelyret@gmail.com>
2016-12-30 09:24:45 +0100

    adjusted layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
   resources :conversations, only: [:index, :show, :destroy] do
     member do
       post :reply, :restore, :mark_as_read
@@ -14,5 +15,6 @@ Rails.application.routes.draw do
   end
 
   resources :messages, only: [:new, :create]
+  resources 'users', only: [:index, :show]
 
 end
