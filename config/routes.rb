a68eeb920f0996983808f2dd3c5e84d18dbc6420
Samuel Rodriguez <samuelyret@gmail.com>
2016-12-30 17:00:19 +0100

    commented out trash in messages as messages can only be deteled from admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
-  resources :conversations, only: [:index, :show, :destroy] do
+  resources :conversations, only: [:index, :show, :destroy, :update] do
     member do
       post :reply, :restore, :mark_as_read
     end
